d77a540efef572a8c50b33ea15e723be09f804cc
Kirit Sælensminde <k@kirit.com>
2014-11-13 11:09:34 +0700

    Fix up make file for fost-android library.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,1 +1,16 @@
-include $(call my-dir)/fost-android-ndk/Android.mk
+JNI := $(call my-dir)
+
+include $(JNI)/fost-android-ndk/Android.mk
+include $(CLEAR_VARS)
+
+include $(TOP_PATH)/opts.mk
+
+LOCAL_PATH := $(JNI)
+LOCAL_MODULE := fost-android
+
+LOCAL_SRC_FILES := \
+	fost-android.cpp
+LOCAL_SHARED_LIBRARIES := \
+    boost-system
+
+include $(BUILD_SHARED_LIBRARY)
