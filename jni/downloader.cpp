1b9f9e17598ce916810cf9dc5743276925f1528c
Kirit Sælensminde <k@kirit.com>
2014-11-14 18:17:52 +0700

    Remove dead code.

--- jni/downloader.cpp
+++ jni/downloader.cpp
@@ -1,9 +1,15 @@
+/*
+    Copyright 2014 Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
 #include <fost/aws>
 #include <fost/insert>
 #include <fost/timer>
-#include <proxy/config.hpp>
-#include <varanus/acanthurus.hpp>
-#include <varanus/obor.hpp>
+#include <fost/config.hpp>
 
 
 using namespace fostlib;
@@ -16,9 +22,7 @@ namespace {
         json result;
         aws::s3::bucket bucket(
             coerce<ascii_printable_string>(
-                coerce<nullable<string>>(config["bucket"])
-                    .value(proxy::c_bucket.value())));
-
+                coerce<nullable<string>>(config["bucket"]).value()));
         timer taken;
         string content(bucket.get(
             coerce<boost::filesystem::wpath>(
@@ -26,22 +30,8 @@ namespace {
         json db_content(json::parse(content));
         insert(result, "downloaded", db_content);
         insert(result, "time-taken", taken.elapsed());
-
-        varanus::db localdb(varanus::local_data(
-            coerce<nullable<string>>(config["db"]).value("configuration"),
-            db_content));
-        fostlib::jsondb::local localdata(localdb());
-        if ( localdata[jcursor()] != db_content ) {
-            insert(result, "refreshed", true);
-            localdata.set(jcursor(), db_content);
-            localdata.commit();
-        } else {
-            insert(result, "refreshed", false);
-        }
         return result;
     }
-    const varanus::obor c_statistics(
-        "proxy.downloader.db.local", s3_into_local_db);
 
 
 }
