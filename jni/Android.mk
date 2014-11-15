84bf9cbb96b8f2b0afea3b0a27c4543815a512f5
Kirit Sælensminde <k@kirit.com>
2014-11-15 12:18:06 +0700

    Include the new required files.

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,12 +9,14 @@ LOCAL_PATH := $(JNI)
 LOCAL_MODULE := fost-android
 
 LOCAL_SRC_FILES := \
-	fost-android.cpp \
-	webserver.cpp
+    fost-android.cpp \
+    setting.cpp \
+    webserver.cpp
 LOCAL_SHARED_LIBRARIES := \
-	boost-system \
+    boost-system \
     fost-core \
     fost-inet \
     fost-urlhandler
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
