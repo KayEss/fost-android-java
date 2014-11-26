b763ee7247df277c68e8d3e954702d6364e18a84
Kirit Sælensminde <k@kirit.com>
2014-11-26 16:21:03 +0700

    Load the assets using the Java Asset class.

--- jni/view-assets.cpp
+++ jni/view-assets.cpp
@@ -18,16 +18,45 @@ namespace {
         }
 
         std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
-            const fostlib::json &config, const fostlib::string &path,
+            const fostlib::json &config, const fostlib::string &requested_path,
             fostlib::http::server::request &req,
             const fostlib::host &
         ) const {
-            fostlib::mime::mime_headers headers;
-            return std::make_pair(new fostlib::text_body(
-                        "<html><head><title>Asset</title></head>"
-                        "<body><p>" + path + "</p></body></html>",
-                    headers, "text/html"),
-                200);
+            fostlib::string path(
+                fostlib::coerce<fostlib::string>(config["asset"]) + requested_path);
+            if ( requested_path.empty() || requested_path.endswith("/") ) {
+                path += "index.html";
+            }
+            JNIEnv *env = fostlib::get_environment();
+            jmethodID jloadBytes(env->GetStaticMethodID(
+                com::felspar::android::Asset,
+                "loadBytes", "(Ljava/lang/String;)[B"));
+            jstring asset(reinterpret_cast<jstring>(
+                env->NewLocalRef(env->NewStringUTF(path.c_str()))));
+            jobject load_result(env->CallStaticObjectMethod(
+                    com::felspar::android::Asset, jloadBytes, asset));
+            jbyteArray *jbytes(reinterpret_cast<jbyteArray*>(&load_result));
+            if ( *jbytes == nullptr ) {
+                return std::make_pair(
+                    new fostlib::text_body(
+                        "<html><head><title>404</title></head>"
+                        "<body><h1>404</h1><pre>" + path + "</pre></body></html>"),
+                    404);
+            } else {
+                std::size_t length(env->GetArrayLength(*jbytes));
+                std::shared_ptr<signed char> bytes(
+                    env->GetByteArrayElements(*jbytes, nullptr),
+                    [env, jbytes](signed char *ptr) {
+                        env->ReleaseByteArrayElements(*jbytes, ptr, 0);
+                    });
+                return std::make_pair(
+                    new fostlib::binary_body(
+                        bytes.get(), bytes.get() + length,
+                        fostlib::mime::mime_headers(),
+                        fostlib::urlhandler::mime_type(
+                            fostlib::coerce<boost::filesystem::wpath>(path))),
+                    200);
+            }
         }
     } c_asset_view;
 }
