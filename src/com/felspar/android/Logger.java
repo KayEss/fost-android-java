1ceaa14ed742c89bc1cf6aa749f924c95ecf8607
Kirit Sælensminde <k@kirit.com>
2014-12-01 09:58:27 +0700

    Add first version of JavaScript logging.

--- src/com/felspar/android/Logger.java
+++ src/com/felspar/android/Logger.java
@@ -12,15 +12,35 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 
-public class Logger {
+public class Logger extends WebChromeClient {
     public static void log(int priority, String tag, String message) {
         Log.println(priority, tag, message);
     }
+
     public static void log(int priority, String tag, Throwable exception) {
         final StringWriter sw = new StringWriter();
         final PrintWriter pw = new PrintWriter(sw, true);
         exception.printStackTrace(pw);
         log(priority, tag, sw.getBuffer().toString());
+    }
+
+    public boolean onConsoleMessage(ConsoleMessage cm) {
+    	int priority = Log.ERROR;
+    	if ( cm.messageLevel() == ConsoleMessage.MessageLevel.DEBUG ) {
+    	    priority = Log.DEBUG;
+    	} else if ( cm.messageLevel() == ConsoleMessage.MessageLevel.LOG ||
+    			cm.messageLevel() == ConsoleMessage.MessageLevel.TIP ) {
+    		priority = Log.INFO;
+    	} else if ( cm.messageLevel() == ConsoleMessage.MessageLevel.WARNING ) {
+    		priority = Log.WARN;
+    	}
+        Logger.log(priority, "JavaScript",
+            cm.message() + " -- From line " +
+            cm.lineNumber() + " of " +
+            cm.sourceId());
+        return true;
     }
 }
